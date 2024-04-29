3c01e336c616a98edd38eb3a02b34a33e176183d
Mailonap543 <augustomailon4@gmail.com>
2024-04-29 14:20:53 -0300

    criacao de classes

---
+++ src/main/java/com/loja/rock/service/ClienteService.java
@@ -0,0 +1,23 @@
+package com.loja.rock.service;
+
+
+import com.loja.rock.entity.ClienteModel;
+
+public class ClienteService {
+
+    
+    public ClienteModel criarCliente(ClienteModel cliente) {
+		return cliente;
+        // Lógica de negócio para criar um novo cliente
+      
+
+    }
+
+	public ClienteModel buscarClientePorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
+}
+
+
---
+++ src/main/java/com/loja/rock/service/ProdutoService.java
@@ -0,0 +1,5 @@
+package com.loja.rock.service;
+
+public class ProdutoService {
+
+}
